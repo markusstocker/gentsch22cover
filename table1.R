6942d451653e0c283458c1bccf0dbdb4730cf142
Markus Stocker <markus.stocker@gmail.com>
2022-11-11 18:21:08 +0100

    reverting

--- table1.R
+++ table1.R
@@ -39,8 +39,7 @@ df2 <- data.frame(summary(lm.mwd.2)$coefficients, check.names=FALSE)
 
 instance <- tp$model_fitting(
   label="Linear mixed model fitting with MWD as response, CC variant as predictor variable, and soil depth as random variable", 
-  has_input_dataset="https://doi.org/10.5281/zenodo.7314152",
-  #has_input_dataset="https://github.com/markusstocker/gentsch22cover/blob/main/df.MWD.csv",
+  has_input_dataset="https://github.com/markusstocker/gentsch22cover/blob/main/df.MWD.csv",
   #has_input_dataset=tuple(df.MWD, "Difference of mean weight diameter between the dry and wet sieving method"),
   has_input_model=tp$statistical_model(
     label="A linear mixed model with MWD as response and CC variant as predictor variable",
